644d9806c7d69b0497de202b5148103a8462a0f7
Renato <renato.rao@bluewin.ch>
2022-03-28 16:11:08 +0200

    Implemented bpTree.Put added some tests and some refactoring

--- kv/keyValueStore.go
+++ kv/keyValueStore.go
@@ -21,6 +21,11 @@ var (
 	ErrInvalidPath = errors.New(Package + " - 'path' is not valid")
 )
 
+const (
+	// Determines maximum amount of leaves per node
+	BRANCHING_FACTOR = 3
+)
+
 type KeyValueStore interface {
 	Get(uint64) ([]byte, error) // Returns an error if the given key is not found
 	Put(uint64, [10]byte) error // Returns an error on inserting same key twice
@@ -34,30 +39,243 @@ type KeyValueStore interface {
 	DeleteStore(string) error                  // Deletes the KeyValueStore. Error if no KeyValueStore at that path
 }
 
-type kvImpl struct {
-	MaxMem int
-	Path   string
+type bpTreeImpl struct {
+	MaxMem           int
+	Path             string
+	root             *Node
+	branching_factor int
 }
 
-func (k kvImpl) Get(key uint64) ([]byte, error) {
+type Node struct {
+	num_keys int
+	ISLEAF   bool
+	keys     [BRANCHING_FACTOR + 1]uint64
+	pointers [BRANCHING_FACTOR + 1]*Node
+	value    [10]byte
+	next     *Node
+}
+
+func (bpTree bpTreeImpl) Get(key uint64) ([]byte, error) {
 	var retValue []byte
 	return retValue, nil
 }
 
-func (k *kvImpl) Put(key uint64, value [10]byte) error {
+func (bpTree *bpTreeImpl) Put(key uint64, value [10]byte) error {
+
+	// Empty bpTree insert at root
+	if bpTree.root == (*Node)(nil) {
+		var newNode Node
+		newNode.keys[0] = key
+		newNode.ISLEAF = true
+		newNode.num_keys = 1
+		bpTree.root = &newNode
+
+		return nil
+	}
+
+	var iterator *Node = bpTree.root
+	var parent *Node
+
+	for iterator.ISLEAF == false {
+		parent = iterator
+
+		for i := 0; i < parent.num_keys; i++ {
+			if key < iterator.keys[i] {
+				iterator = iterator.pointers[i]
+				break
+			}
+
+			if i == iterator.num_keys-1 {
+				iterator = iterator.pointers[i+1]
+				break
+			}
+		}
+	}
+
+	if iterator.num_keys < bpTree.branching_factor {
+		i := 0
+		for key > iterator.keys[i] && i < iterator.num_keys {
+			i++
+		}
+
+		for j := iterator.num_keys; j > i; j-- {
+			iterator.keys[j] = iterator.keys[j-1]
+		}
+
+		iterator.keys[i] = key
+		iterator.num_keys++
+		iterator.pointers[iterator.num_keys] = iterator.pointers[iterator.num_keys-1]
+		iterator.pointers[iterator.num_keys-1] = nil
+	} else {
+		var newLeaf Node
+		var copyKeys [BRANCHING_FACTOR + 2]uint64
+
+		for i := 0; i < BRANCHING_FACTOR; i++ {
+			copyKeys[i] = iterator.keys[i]
+		}
+
+		// Get to insertion point
+		i := 0
+		for key > copyKeys[i] && i < BRANCHING_FACTOR {
+			i++
+		}
+
+		for j := BRANCHING_FACTOR + 1; j > i; j-- {
+			copyKeys[j] = copyKeys[j-1]
+		}
+
+		copyKeys[i] = key
+		newLeaf.ISLEAF = true
+		L := (BRANCHING_FACTOR + 1) / 2
+		iterator.num_keys = L
+		newLeaf.num_keys = BRANCHING_FACTOR + 1 - L
+
+		iterator.pointers[iterator.num_keys] = &newLeaf
+		newLeaf.pointers[newLeaf.num_keys] = iterator.pointers[BRANCHING_FACTOR]
+		iterator.pointers[BRANCHING_FACTOR] = nil
+
+		for i := 0; i < iterator.num_keys; i++ {
+			iterator.keys[i] = copyKeys[i]
+		}
+
+		k := iterator.num_keys
+		for i := 0; i < newLeaf.num_keys; i++ {
+			newLeaf.keys[i] = copyKeys[k]
+			k++
+		}
+
+		if iterator == bpTree.root {
+			var newRoot Node
+
+			newRoot.keys[0] = newLeaf.keys[0]
+			newRoot.pointers[0] = iterator
+			newRoot.pointers[1] = &newLeaf
+			newRoot.ISLEAF = false
+			newRoot.num_keys = 1
+			bpTree.root = &newRoot
+		} else {
+			internalInsertion(newLeaf.keys[0], parent, &newLeaf, bpTree.root)
+		}
+	}
 	return nil
 }
 
-func (k *kvImpl) Delete(key uint64) error {
+func internalInsertion(key uint64, iterator *Node, child *Node, root *Node) error {
+
+	// Enough space to add a new key
+	if iterator.num_keys < BRANCHING_FACTOR {
+		i := 0
+		for key > iterator.keys[i] && i < iterator.num_keys {
+			i++
+		}
+
+		// Make space for new key and pointer
+		for j := iterator.num_keys; j > i; j-- {
+			iterator.keys[j] = iterator.keys[j-1]
+		}
+		for j := iterator.num_keys + 1; j > i; j-- {
+			iterator.pointers[j] = iterator.pointers[j-1]
+		}
+
+		iterator.keys[i] = key
+		iterator.num_keys++
+		iterator.pointers[i+1] = child
+
+	} else { // Node is full, need to split
+		var newNode Node
+
+		var copyKeys [BRANCHING_FACTOR + 1]uint64
+		var copyPointers [BRANCHING_FACTOR + 2]*Node
+
+		// TODO Refactor
+		for i := 0; i < BRANCHING_FACTOR; i++ {
+			copyKeys[i] = iterator.keys[i]
+		}
+
+		for i := 0; i < BRANCHING_FACTOR+1; i++ {
+			copyPointers[i] = iterator.pointers[i]
+		}
+
+		i := 0
+		for key > copyKeys[i] && i < BRANCHING_FACTOR {
+			i++
+		}
+
+		for j := BRANCHING_FACTOR + 1; j > i; j-- {
+			copyKeys[j] = copyKeys[j-1]
+		}
+		copyKeys[i] = key
+		for j := BRANCHING_FACTOR + 2; j > i; j-- {
+			copyPointers[j] = copyPointers[j-1]
+		}
+
+		copyPointers[i+1] = child
+		newNode.ISLEAF = false
+		var L = (BRANCHING_FACTOR + 1) / 2
+		iterator.num_keys = L
+
+		newNode.num_keys = BRANCHING_FACTOR - L
+
+		var k = iterator.num_keys + 1
+		for i := 0; i < newNode.num_keys; i++ {
+			newNode.keys[i] = copyKeys[k]
+			k++
+		}
+
+		k = iterator.num_keys + 1
+		for i := 0; i < newNode.num_keys+1; i++ {
+			newNode.pointers[i] = copyPointers[k]
+			k++
+		}
+
+		if iterator == root {
+			var newRoot Node
+			newRoot.keys[0] = iterator.keys[iterator.num_keys]
+			newRoot.pointers[0] = iterator
+			newRoot.pointers[1] = &newNode
+			newRoot.ISLEAF = false
+			newRoot.num_keys = 1
+			root = &newRoot
+		} else {
+			internalInsertion(iterator.keys[iterator.num_keys], findParent(root, iterator), &newNode, root)
+		}
+	}
+
+	return nil
+}
+
+func findParent(iterator *Node, child *Node) *Node {
+	var parent *Node
+
+	if iterator.ISLEAF || iterator.pointers[0].ISLEAF {
+		return nil
+	}
+
+	for i := 0; i < iterator.num_keys+1; i++ {
+		if iterator.pointers[i] == child {
+			parent = iterator
+			return parent
+		} else {
+			parent = findParent(iterator.pointers[i], child)
+			if parent != nil {
+				return parent
+			}
+		}
+	}
+	return parent
+}
+
+func (k *bpTreeImpl) Delete(key uint64) error {
 	return nil
 }
 
 // ScanRange Returns all values with keys ranging [begin, end) (i.e. range with begin, but without end)
-func (k *kvImpl) ScanRange(begin uint64, end uint64) [][10]byte {
+func (k *bpTreeImpl) ScanRange(begin uint64, end uint64) [][10]byte {
 	return nil
 }
 
-func (k *kvImpl) Create(path string, size int) (*kvImpl, error) {
+func (k *bpTreeImpl) Create(path string, size int) (*bpTreeImpl, error) {
+
 	k.MaxMem = 1 << (10 * 3) // 1 GB
 	k.Path = "."             // create in local directory
 
@@ -68,17 +286,23 @@ func (k *kvImpl) Create(path string, size int) (*kvImpl, error) {
 		fmt.Println(path)
 		return nil, ErrInvalidPath
 	}
+
+	// Create root node
+	var bpTree bpTreeImpl
+	bpTree.branching_factor = BRANCHING_FACTOR
+	bpTree.root = nil
+
+	return &bpTree, nil
+}
+
+func Open(path string) (*bpTreeImpl, error) {
 	return nil, nil
 }
 
-func Open(path string) (*kvImpl, error) {
-	return nil, nil
-}
-
-func (k *kvImpl) Close() error {
+func (k *bpTreeImpl) Close() error {
 	return nil
 }
 
-func DeleteStore(path string) error {
+func (k *bpTreeImpl) DeleteStore(path string) error {
 	return nil
 }
--- kv/keyValueStore_test.go
+++ kv/keyValueStore_test.go
@@ -1,6 +1,7 @@
 package kv
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,28 +16,56 @@ var (
 	Test{function}_{scenario}_{expectation}
 ***/
 
-func setupTestDB(path string, size int) (*kvImpl, error) {
-	var kvImpl kvImpl
-	return kvImpl.Create(path, size)
+func setupTestDB(path string, size int) (*bpTreeImpl, error) {
+	var bpTreeImpl bpTreeImpl
+	return bpTreeImpl.Create(path, size)
 }
 
-func closeTestDb(t *testing.T, kvImpl *kvImpl) {
-	assert.Nil(t, kvImpl.Close())
+func closeTestDb(t *testing.T, bpTreeImpl *bpTreeImpl) {
+	assert.Nil(t, bpTreeImpl.Close())
+}
+
+func printBPTree(bpTree *bpTreeImpl, t *testing.T) {
+	if bpTree.root == nil {
+		fmt.Println("Empty tree.")
+		return
+	}
+
+	printNode(bpTree.root, t)
+}
+
+func printNode(cursor *Node, t *testing.T) {
+
+	if cursor != nil {
+		// Print keys
+		fmt.Print("[")
+		for i := 0; i < cursor.num_keys; i++ {
+			fmt.Print(cursor.keys[i], " | ")
+		}
+		fmt.Print("]")
+		fmt.Println()
+		// Print Values for Leaf Nodes
+		if !cursor.ISLEAF {
+			for i := 0; i < cursor.num_keys+1; i++ {
+				printNode(cursor.pointers[i], t)
+			}
+		}
+	}
 }
 
 func TestCreate(t *testing.T) {
 	const defPath = "." // create in local directory
+	bpTree, err := setupTestDB(defPath, mem)
 
-	_, err := setupTestDB(defPath, mem)
-
+	printBPTree(bpTree, t)
 	assert.Equal(t, nil, err, "Creation failed")
 }
 
 func TestCreate_SizeTooBig_Fail(t *testing.T) {
 	const memLarge = 1 << (10 * 4) // 10 GB > 1GB (MaxMem)
 
-	kvImpl, err := setupTestDB(".", memLarge)
-	defer closeTestDb(t, kvImpl)
+	bpTreeImpl, err := setupTestDB(".", memLarge)
+	defer closeTestDb(t, bpTreeImpl)
 
 	assert.EqualError(t, err, ErrOutOfRange.Error())
 }
@@ -44,11 +73,11 @@ func TestCreate_SizeTooBig_Fail(t *testing.T) {
 func TestCreate_NoPath_Fail(t *testing.T) {
 	const invalidPath = ""
 
-	kvImpl, err := setupTestDB(invalidPath, mem)
-	defer closeTestDb(t, kvImpl)
+	bpTreeImpl, err := setupTestDB(invalidPath, mem)
+	defer closeTestDb(t, bpTreeImpl)
 
 	assert.EqualError(t, err, ErrInvalidPath.Error())
-	assert.Nil(t, kvImpl.Close())
+	assert.Nil(t, bpTreeImpl.Close())
 }
 
 func TestOpen(t *testing.T) {
@@ -57,135 +86,171 @@ func TestOpen(t *testing.T) {
 	_, err := setupTestDB(defPath, mem)
 	assert.Equal(t, nil, err, "Creation failed")
 
-	kvImpl, err := Open(defPath)
-	assert.NotNil(t, kvImpl, "Unable to open KV store")
-	assert.Nil(t, kvImpl.Close())
+	bpTreeImpl, err := Open(defPath)
+	assert.NotNil(t, bpTreeImpl, "Unable to open KV store")
+	assert.Nil(t, bpTreeImpl.Close())
 }
 
 func TestDeleteStore(t *testing.T) {
 	const defPath = "." // create in local directory
 
-	kvImpl, err := setupTestDB(defPath, mem)
+	bpTreeImpl, err := setupTestDB(defPath, mem)
 	assert.Equal(t, nil, err, "Creation failed")
 
-	err = kvImpl.DeleteStore(defPath)
+	err = bpTreeImpl.DeleteStore(defPath)
 	assert.Nil(t, err)
 }
 
 func TestDeleteStore_CannotBeOpened_Fail(t *testing.T) {
 	const defPath = "." // create in local directory
 
-	kvImpl, err := setupTestDB(defPath, mem)
+	bpTreeImpl, err := setupTestDB(defPath, mem)
 	assert.Equal(t, nil, err, "Creation failed")
 
-	err = DeleteStore(defPath)
+	err = bpTreeImpl.DeleteStore(defPath)
 	assert.Nil(t, err)
 
 	_, err = Open(defPath)
 	assert.EqualError(t, err, ErrInvalidPath.Error())
-	assert.Nil(t, kvImpl.Close())
+	assert.Nil(t, bpTreeImpl.Close())
 }
 
 func TestDeleteStore_NoPath_Fail(t *testing.T) {
 	const defPath = "." // create in local directory
+	bpTreeImpl, _ := setupTestDB(defPath, mem)
 
-	err := DeleteStore(defPath)
+	err := bpTreeImpl.DeleteStore(defPath)
 	assert.NotNil(t, err)
 	assert.EqualError(t, err, ErrInvalidPath.Error())
 }
 
 func TestPut(t *testing.T) {
-	kvImpl, _ := setupTestDB(".", mem)
-	defer closeTestDb(t, kvImpl)
+	bpTreeImpl, _ := setupTestDB(".", mem)
+	// defer closeTestDb(t, bpTreeImpl)
 
-	err := kvImpl.Put(123, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
+	err := bpTreeImpl.Put(123, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
 
 	assert.Equal(t, nil, err, "Insert failed")
-	assert.Nil(t, kvImpl.Close())
+	assert.Nil(t, bpTreeImpl.Close())
+	printBPTree(bpTreeImpl, t)
+}
+
+func TestPut_ComplexInsert(t *testing.T) {
+	bpTreeImpl, _ := setupTestDB(".", mem)
+	// defer closeTestDb(t, bpTreeImpl)
+
+	bpTreeImpl.Put(123, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
+	bpTreeImpl.Put(22, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
+	err := bpTreeImpl.Put(1, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
+
+	printBPTree(bpTreeImpl, t)
+	assert.Equal(t, nil, err, "Insert failed")
+	assert.Nil(t, bpTreeImpl.Close())
+}
+
+func TestPut_Insert_SplitNode(t *testing.T) {
+	bpTreeImpl, _ := setupTestDB(".", mem)
+	// defer closeTestDb(t, bpTreeImpl)
+
+	bpTreeImpl.Put(123, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
+	bpTreeImpl.Put(22, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
+	bpTreeImpl.Put(1, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
+	err := bpTreeImpl.Put(59, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
+
+	assert.Equal(t, nil, err, "Insert failed")
+	assert.Nil(t, bpTreeImpl.Close())
+	printBPTree(bpTreeImpl, t)
 }
 
 func TestPut_SameKeyTwice_Fails(t *testing.T) {
-	kvImpl, _ := setupTestDB(".", mem)
-	defer closeTestDb(t, kvImpl)
-	kvImpl.Put(123, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
+	bpTreeImpl, _ := setupTestDB(".", mem)
+	defer closeTestDb(t, bpTreeImpl)
+	bpTreeImpl.Put(123, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
 
 	// Different value same key
-	err := kvImpl.Put(123, [10]byte{1, 0, 0, 0, 0, 1, 1, 1, 1, 1})
+	err := bpTreeImpl.Put(123, [10]byte{1, 0, 0, 0, 0, 1, 1, 1, 1, 1})
 
 	assert.Equal(t, nil, err, "Insert failed")
 
-	assert.Nil(t, kvImpl.Close())
+	assert.Nil(t, bpTreeImpl.Close())
 }
 
 func TestGet(t *testing.T) {
-	kvImpl, _ := setupTestDB(".", mem)
-	defer closeTestDb(t, kvImpl)
-	kvImpl.Put(123, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
+	bpTreeImpl, _ := setupTestDB(".", mem)
+	defer closeTestDb(t, bpTreeImpl)
+	bpTreeImpl.Put(123, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
 
-	value, err := kvImpl.Get(123)
+	value, err := bpTreeImpl.Get(123)
 
 	assert.Equal(t, err, nil, "An error occured while getting key")
 	assert.Equal(t, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1}, value, "Key was not present")
 }
 
 func TestGet_DeletedKey_Error(t *testing.T) {
-	kvImpl, _ := setupTestDB(".", mem)
-	defer closeTestDb(t, kvImpl)
-	kvImpl.Put(123, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
-	kvImpl.Delete(123)
+	bpTreeImpl, _ := setupTestDB(".", mem)
+	defer closeTestDb(t, bpTreeImpl)
+	bpTreeImpl.Put(123, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
+	bpTreeImpl.Delete(123)
 
-	value, err := kvImpl.Get(123)
+	value, err := bpTreeImpl.Get(123)
 
 	assert.Equal(t, err, nil, "An error occured while getting key")
 	assert.Equal(t, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1}, value, "Key was not present")
 }
 
 func TestDelete(t *testing.T) {
-	kvImpl, _ := setupTestDB(".", mem)
-	defer closeTestDb(t, kvImpl)
+	bpTreeImpl, _ := setupTestDB(".", mem)
+	defer closeTestDb(t, bpTreeImpl)
 
-	err := kvImpl.Delete(123)
+	err := bpTreeImpl.Delete(123)
 
 	assert.Equal(t, nil, err)
 }
 
 func TestDelete_DeleteTwice_Fail(t *testing.T) {
-	kvImpl, _ := setupTestDB(".", mem)
-	defer closeTestDb(t, kvImpl)
-	kvImpl.Delete(123)
+	bpTreeImpl, _ := setupTestDB(".", mem)
+	defer closeTestDb(t, bpTreeImpl)
+	bpTreeImpl.Delete(123)
 
-	err := kvImpl.Delete(123)
+	err := bpTreeImpl.Delete(123)
 
 	assert.NotEqual(t, nil, err, "Deleting twice should raise an error")
 }
 
 func TestScan(t *testing.T) {
-	kvImpl, _ := setupTestDB(".", mem)
-	defer closeTestDb(t, kvImpl)
+	bpTreeImpl, _ := setupTestDB(".", mem)
+	defer closeTestDb(t, bpTreeImpl)
 	val123 := [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1}
 	val127 := [10]byte{0, 0, 0, 0, 1, 0, 1, 0, 1, 0}
 	val130 := [10]byte{0, 0, 0, 0, 1, 1, 1, 1, 1, 1}
-	kvImpl.Put(123, val123)
-	kvImpl.Put(127, val127)
-	kvImpl.Put(130, val130)
+	bpTreeImpl.Put(123, val123)
+	bpTreeImpl.Put(127, val127)
+	bpTreeImpl.Put(130, val130)
 
-	results := kvImpl.ScanRange(122, 128)
+	results := bpTreeImpl.ScanRange(122, 128)
 	assert.Equal(t, len(results), 2, "Should have exactly two values in range")
 	assert.Equal(t, results[0], val123, "Values should be ordered correctly")
 	assert.Equal(t, results[1], val127, "Values should be ordered correctly")
 }
 
-func TestScan_TestRangeIncludesStarTExcludesEndOfRange(t *testing.T) {
-	kvImpl, _ := setupTestDB(".", mem)
-	defer closeTestDb(t, kvImpl)
+func TestScan_TestRangeIncludesStartExcludesEndOfRange(t *testing.T) {
+	bpTreeImpl, _ := setupTestDB(".", mem)
+	defer closeTestDb(t, bpTreeImpl)
 	val123 := [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1}
 	val127 := [10]byte{0, 0, 0, 0, 1, 0, 1, 0, 1, 0}
 	val130 := [10]byte{0, 0, 0, 0, 1, 1, 1, 1, 1, 1}
-	kvImpl.Put(123, val123)
-	kvImpl.Put(127, val127)
-	kvImpl.Put(130, val130)
+	bpTreeImpl.Put(123, val123)
+	bpTreeImpl.Put(127, val127)
+	bpTreeImpl.Put(130, val130)
 
-	results := kvImpl.ScanRange(123, 127)
+	results := bpTreeImpl.ScanRange(123, 127)
 	assert.Equal(t, len(results), 1, "Should have exactly on value in range")
 	assert.Equal(t, results[0], val123, "Scan start should be included in result")
+}
+
+func TestPrintBPTree(t *testing.T) {
+	bpTreeImpl, _ := setupTestDB(".", mem)
+	defer closeTestDb(t, bpTreeImpl)
+
+	bpTreeImpl.Put(5, [10]byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1})
 }
